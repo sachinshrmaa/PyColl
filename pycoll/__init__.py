56bd7684665df4a1a7b77d728f7e0f6e62f8fefa
Sachin Sharma <39833358+sachinshrmaa@users.noreply.github.com>
2020-05-08 23:41:49 +0530

    Add files via upload
    Added Version: 0.0.1 to PyPi

---
+++ pycoll/__init__.py
@@ -0,0 +1,20 @@
+
+#To fing the area of Square
+def area_square(side):
+    area = side * side
+    return print(area)
+
+#To fing the area of Rectangle
+def area_rectangle(length, breadth):
+    area = length * breadth
+    return print(area)
+
+#To fing the area of Triangle
+def area_triangle(base, height):
+    area = 1/2 * base * height
+    return print(area)
+
+#To fing the area of Parallelogram
+def area_parallelogram(base, height):
+    area =  base * height
+    return print(area)

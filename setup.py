0fc2b591062f3aedbfb85e542262045931a3a284
Sachin Sharma <contact@sachinshrma.com>
2020-05-09 14:10:24 +0530

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+ 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+ 
+setuptools.setup(
+   
+    name="pycoll",
+    version="0.0.1",
+    author="Sachin Sharma",
+    author_email="contact@sachinshrmaa.com",
+    description="Find Area of Square, Rectangle, Triangle and Parallelogram ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sachinshrmaa/pycoll",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
